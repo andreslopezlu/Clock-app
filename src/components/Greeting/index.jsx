4f915c2fb8e816f9f27f59a6104a7eca94fe5b1f
andreslopezlu <andreslopezlu@gmail.com>
2025-01-30 21:58:20 -0500

    add functionality to location component

--- src/components/Greeting/index.jsx
+++ src/components/Greeting/index.jsx
@@ -3,17 +3,21 @@ import { useState } from "react"
 const Greeting = ({time}) => {
 
     const stablihTimeOfTheDay = () => {
-        const hour = parseInt(time.slice(0, 2))
-        let result
-        if ( 0 <= hour && hour <= 12) {
-            result = "Morning"
-        } if ( 12 < hour && hour <= 18 ) {
-            result = "Afternoon"
-        } else {
-            result = "Evening"
+        try {
+            const hour = parseInt(time.slice(0, 2))
+            let result
+            if ( 0 <= hour && hour < 12) {
+                result = "MORNING"
+            } if ( 12 <= hour && hour < 18 ) {
+                result = "AFTERNOON"
+            } else {
+                result = "EVENING"
+            }
+            console.log(result);
+            return result
+        } catch(error){
+            console.log(error)
         }
-        console.log(result);
-        return result
     }
 
     const timeOfTheDay = stablihTimeOfTheDay()
@@ -21,7 +25,7 @@ const Greeting = ({time}) => {
     return (
         <div className="greeting">
             <img src="../../assets/desktop/icon-sun.svg" alt="morning-icon" />
-            <p>{`${timeOfTheDay}, IT’S CURRENTLY`}</p>
+            <p>{`GOOD ${timeOfTheDay}, IT’S CURRENTLY`}</p>
         </div>
     )   
 }
