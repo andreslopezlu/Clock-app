365ddae70eb5d41c1ad1d5f41c766c7254f64c82
andreslopezlu <andreslopezlu@gmail.com>
2025-03-12 18:34:22 -0500

    modify imports

--- src/components/Cities/index.jsx
+++ src/components/Cities/index.jsx
@@ -4,7 +4,7 @@ const Cities = ({citiesData}) => {
 
     let data = citiesData?.data || []
 
-    const result = data.map((item) => {
+    const renderCities = data.map((item) => {
         const id = item.id
         const city = item.city
         const country = item.countryCode
@@ -12,9 +12,10 @@ const Cities = ({citiesData}) => {
             <City key={id} id={id} city={city} country={country} />
         )
     }) 
+    
     return (
         <ul>
-            {result}
+            {renderCities}
         </ul>
     )
 }
--- src/components/Favorites/index.jsx
+++ src/components/Favorites/index.jsx
@@ -1,38 +1,54 @@
 import { useEffect, useState } from 'react'
+
+import City from '../Cities/components/City'
+
 import useCityDetailsApi from '../../hooks/useCityDetailsApi'
+
 import styles from './Favorites.module.css'
 
 const Favorites = ({favoritesData}) => {
 
-    const [result, setResult] = useState([])
+    const [favorites, setFavorites] = useState([])
 
     const {getCityDetailData} = useCityDetailsApi()
 
-    console.log('favoritos', favoritesData)
-
     useEffect(() => {
+        console.log('1', favorites)
         let time = 0
+        let result = []
         function fetchData() {
             for (const i in favoritesData){
                 setTimeout(() => {
                     const item = getCityDetailData(favoritesData[i])
-                    console.log(item)
-                    setResult([...result, item])
+                    item.then((response) => {
+                        result.push(response.data)
+                    })
                 }, time);
                 time += 1500
             }
-        } 
+        }
+        console.log('2', favorites) 
         fetchData()
+        setFavorites(result)
+        console.log('3', favorites) 
     }, [favoritesData]);
     
-    console.log('resultado', result)
+    console.log('4', favorites)
 
+    const renderFavorites = favorites.map((item) => {
+        const id = item.id
+        const city = item.city
+        const country = item.countryCode
+        return (
+            <City key={id} id={id} city={city} country={country} />
+        )
+    }) 
+    
     return (
-        <div>
-            {result.map((city) => {
-                <span key={city.id}>{city.name}</span>
-            })}
-        </div>
+        <ul>
+            {renderFavorites}
+            <li>hola</li>
+        </ul>
     )
 }
 
