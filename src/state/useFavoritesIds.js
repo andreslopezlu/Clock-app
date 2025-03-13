691a320e17984e53e5c9c53b06eb4c2eee02e51d
andreslopezlu <andreslopezlu@gmail.com>
2025-03-13 12:47:11 -0500

    add and modify  constants

--- src/state/useFavoritesIds.js
+++ src/state/useFavoritesIds.js
@@ -1,25 +1,25 @@
 import { create } from "zustand"
 
-import { FAVORITES_LOCAL_STORAGE } from "../utils/constants"
+import { FAVORITES_IDS_LOCAL_STORAGE } from "../utils/constants"
 
 const useFavoritesIds = create((set, get) => {
 
-    const initialFavorites = JSON.parse(localStorage.getItem(FAVORITES_LOCAL_STORAGE)) || []
+    const initialFavorites = JSON.parse(localStorage.getItem(FAVORITES_IDS_LOCAL_STORAGE)) || []
     const initialTotal = initialFavorites.length
 
-    if(!localStorage.getItem(FAVORITES_LOCAL_STORAGE)) {
-        localStorage.setItem(FAVORITES_LOCAL_STORAGE, JSON.stringify([]))
+    if(!localStorage.getItem(FAVORITES_IDS_LOCAL_STORAGE)) {
+        localStorage.setItem(FAVORITES_IDS_LOCAL_STORAGE, JSON.stringify([]))
     }
 
     return {
         favoritesData: initialFavorites,
         totalFavorites: initialTotal,
         saveDeleteFavorites: (id) => {
-            const favorites = JSON.parse(localStorage.getItem(FAVORITES_LOCAL_STORAGE))
+            const favorites = JSON.parse(localStorage.getItem(FAVORITES_IDS_LOCAL_STORAGE))
             const favoriteIndex = favorites.indexOf(id)
             favorites.includes(id) ? favorites.splice(favoriteIndex, 1) : favorites.push(id)
             const total = favorites.length
-            localStorage.setItem(FAVORITES_LOCAL_STORAGE, JSON.stringify(favorites));
+            localStorage.setItem(FAVORITES_IDS_LOCAL_STORAGE, JSON.stringify(favorites));
             set(() => ({
                 favoritesData: favorites,
                 totalFavorites: total
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
-export const FAVORITES_LOCAL_STORAGE = 'favorites'
+export const FAVORITES_IDS_LOCAL_STORAGE = 'favorites'
+export const FAVORITES_DATA_LOCAL_STORAGE = 'favorites-data'
 export const FAVORITES_LIMIT = 3
